ce9437358a33d51c032ddb19871043f24923b611
basarYargici <ibrahimbyargici@gmail.com>
2021-02-07 14:46:43 +0300

    donation recieved page edited

--- lib/screens/donationPage/donation_received.dart
+++ lib/screens/donationPage/donation_received.dart
@@ -1,15 +1,10 @@
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hackathon_bread_donate/config/checkIsLoggedIn.dart';
 import 'package:flutter_hackathon_bread_donate/config/constants.dart';
 import 'package:flutter_hackathon_bread_donate/config/size_config.dart';
-import 'package:flutter_hackathon_bread_donate/models/baker_system_model.dart';
 import 'package:flutter_hackathon_bread_donate/screens/splash/components/splash_text.dart';
-import 'package:flutter_hackathon_bread_donate/service/service.dart';
 import 'package:flutter_hackathon_bread_donate/widgets/lottie_widget.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // TODO's baker should be send to HOMESCREEN
 class DonationReceived extends StatefulWidget {
@@ -20,8 +15,6 @@ class DonationReceived extends StatefulWidget {
 }
 
 class _DonationReceivedState extends State<DonationReceived> {
-
-
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -50,7 +43,7 @@ class _DonationReceivedState extends State<DonationReceived> {
         Expanded(
             flex: 2,
             child: SplashText(
-              appName: "THANKS",
+              appName: 'TEŞEKKÜRLER',
             )),
       ],
     );
